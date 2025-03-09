45c8d4966a7127adda02cf690d430fd5a1ff5ab6
agustinMontoya2 <agus7_montoya@hotmail.com>
2025-03-09 14:00:50 -0300

    localstorage in callback

--- src/components/orders/orders-component.tsx
+++ src/components/orders/orders-component.tsx
@@ -1,8 +1,16 @@
 import { IOrder } from "@/interfaces/user"
 import { OrderComponent } from "./order-component"
+import { useEffect, useState } from "react"
 
 export const OrdersComponent = () => {
-    const orders = localStorage.getItem('orders') ? JSON.parse(localStorage.getItem('orders') as string) : []
+    const [orders, setOrders] = useState<IOrder[]>([])
+    const handleGetOrders = () => {
+        const orders = localStorage.getItem('orders') ? JSON.parse(localStorage.getItem('orders') as string) : []
+        setOrders(orders)
+    }
+    useEffect(() => {
+        handleGetOrders()
+    }, [])
     return (
         <div className="bg-whiteTransparent text-[rgb(0,0,0)] w-full h-[80%] border-solid border-[rgb(0,0,0)] border-2 rounded-2xl">
         {orders.map((order: IOrder) => {
