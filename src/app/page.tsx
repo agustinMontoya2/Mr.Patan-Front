303ba30120c9d84fd388a5fa1db34c7c04206a23
agustinMontoya2 <agus7_montoya@hotmail.com>
2025-03-10 15:39:50 -0300

    restore home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,16 @@ import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  // const handleGetUser = async () => {
-  //   const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null;
-  //   router.push(user ? "/perfil" : "/inicio");
-  // }
+  const handleGetUser = async () => {
+    const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") as string) : null;
+    router.push(user ? "/perfil" : "/inicio");
+  }
   
-  // useEffect(() => {
-  //   handleGetUser()
-  // }, [handleGetUser])
-  return ( <InputDate />)
-{/* <div className="flex flex-col items-center justify-between h-screen w-screen">
-
-</div> */}
+  useEffect(() => {
+    handleGetUser()
+  }, [handleGetUser])
+  return ( 
+    <div className="flex flex-col items-center justify-between h-screen w-screen">
+    </div> )
   ;
 }
